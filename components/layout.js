b61cf6d726b54472dda65c21cf4d327f0de0da6d
Joseph Knowles <joejknowles@gmail.com>
2017-03-27 12:09:15 +0100

    add material-ui

--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,22 @@
+import HEAD from 'next/head';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import '../lib/tapEvents.js';
+
 export default ({ children }) => (
   <div>
-    <h1>Joe Knowles</h1>
-    { children }
+    <HEAD>
+      <link rel='stylesheet' href='static/app.css' />
+    </HEAD>
+    <MuiThemeProvider>
+      <div>
+        <AppBar
+          title='Joe Knowles'
+          showMenuIconButton={ false }/>
+        <main>
+          { children }
+        </main>
+      </div>
+    </MuiThemeProvider>
   </div>
 );
